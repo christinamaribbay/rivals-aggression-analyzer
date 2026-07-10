3a98ce47fa39c6705e22d9c873c0506147712cc4
Rei <mma79665@gmail.com>
2026-07-10 13:40:20 -0400

    added hash table

--- include/hash_table.cpp
+++ include/hash_table.cpp
@@ -3,3 +3,67 @@
 //
 
 #include "hash_table.h"
+
+HashTable::HashTable(int size) {
+    this->buckets = size;
+    table.assign(buckets, nullptr);
+}
+
+
+HashTable::~HashTable() {
+    for (int i = 0; i < buckets; i++) {
+        HashNode* current = table[i];
+        while (current != nullptr) {
+            HashNode* prev = current;
+            current = current->next;
+            delete prev;
+        }
+    }
+}
+
+bool HashTable::isEmpty() {
+    for (int i = 0; i < buckets; i++) {
+        if (table[i] != nullptr) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int HashTable::hashFunction(string key) {
+    unsigned int hashcode = 0;
+    for (int i = 0; i < key.length(); i++) {
+        hashcode = (hashcode * 31 + key[i]) % buckets;
+    }
+    return hashcode;
+}
+
+void HashTable::insertWord(string key, float value) {
+    int hashValue = hashFunction(key);
+    HashNode* current = table[hashValue];
+    while (current != nullptr) {
+        if (current->word == key) {
+            current->aggressionFactor = value;
+            return;
+        }
+        current = current->next;
+    }
+
+    HashNode* newNode = new HashNode(key, value);
+    newNode->next = table[hashValue];
+    table[hashValue] = newNode;
+}
+
+float HashTable::searchWord(string key) {
+    int index = hashFunction(key);
+    HashNode* current = table[index];
+    while (current != nullptr) {
+        if (current->word == key) {
+            return current->aggressionFactor;
+        }
+        current = current->next;
+    }
+
+    return -1.0f;
+}
--- include/hash_table.h
+++ include/hash_table.h
@@ -4,5 +4,34 @@
 
 #ifndef RIVALS_AGGRESSION_ANALYZER_HASH_TABLE_H
 #define RIVALS_AGGRESSION_ANALYZER_HASH_TABLE_H
+#pragma once
+#include "Player.h"
+#include <vector>
+
+struct HashNode {
+    string word;
+    float aggressionFactor;
+    HashNode* next = nullptr;
+
+    HashNode(string word, float aggressionFactor) {
+        this->word = word;
+        this->aggressionFactor = aggressionFactor;
+        this->next = nullptr;
+    };
+};
+
+class HashTable {
+    int buckets;
+    vector<HashNode*> table;
+
+public:
+    HashTable(int size = 31);
+    ~HashTable();
+    bool isEmpty();
+    int hashFunction(string key);
+    void insertWord(string key, float value);
+    float searchWord(string key);
+
+};
 
 #endif //RIVALS_AGGRESSION_ANALYZER_HASH_TABLE_H
